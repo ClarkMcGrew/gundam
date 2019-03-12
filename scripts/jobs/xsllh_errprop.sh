b1f7040e4b61f6f2263e92d8c51a4203fe727815
Lucie Maret <lmaret@cca005.in2p3.fr>
2019-03-12 16:46:23 +0100

    Add scripts to run jobs

---
+++ scripts/jobs/xsllh_errprop.sh
@@ -0,0 +1,45 @@
+#! /bin/bash  
+
+export WORKDIR='/sps/t2k/lmaret/softwares/xsLLhFitterLM/'
+
+cd $WORKDIR/scripts/jobs/toSubmit
+rm job_xsllh_errprop_*.sh
+
+# Write scripts with jobs to submit
+
+# Asimov fit
+echo -e "cd $WORKDIR; source setup.sh; xsllhCalcXsec -j $WORKDIR/inputs/fgd1fgd2Fit/errprop_fit1_asimov.json" >> job_xsllh_errprop_fit1_asimov.sh
+
+# Fake data with parameter thrown and statistical fluctuations
+echo -e "cd $WORKDIR; source setup.sh; xsllhCalcXsec -j $WORKDIR/inputs/fgd1fgd2Fit/errprop_fit3_statFluc.json" >> job_xsllh_errprop_fit3_statFluc.sh
+
+# Fake data with altered carbon or oxygen content
+echo -e "cd $WORKDIR; source setup.sh; xsllhCalcXsec -j $WORKDIR/inputs/fgd1fgd2Fit/errprop_fit1_fakedata_m30Carbon.json" >> job_xsllh_errprop_fit1_fakedata_m30Carbon.sh
+echo -e "cd $WORKDIR; source setup.sh; xsllhCalcXsec -j $WORKDIR/inputs/fgd1fgd2Fit/errprop_fit1_fakedata_p30Carbon.json" >> job_xsllh_errprop_fit1_fakedata_p30Carbon.sh
+echo -e "cd $WORKDIR; source setup.sh; xsllhCalcXsec -j $WORKDIR/inputs/fgd1fgd2Fit/errprop_fit1_fakedata_m30Oxygen.json" >> job_xsllh_errprop_fit1_fakedata_m30Oxygen.sh
+echo -e "cd $WORKDIR; source setup.sh; xsllhCalcXsec -j $WORKDIR/inputs/fgd1fgd2Fit/errprop_fit1_fakedata_p30Oxygen.json" >> job_xsllh_errprop_fit1_fakedata_p30Oxygen.sh
+
+# Fake data with GENIE and NuWro inputs
+echo -e "cd $WORKDIR; source setup.sh; xsllhCalcXsec -j $WORKDIR/inputs/fgd1fgd2Fit/errprop_fit1_fakedata_genie.json" >> job_xsllh_errprop_fit1_fakedata_genie.sh
+echo -e "cd $WORKDIR; source setup.sh; xsllhCalcXsec -j $WORKDIR/inputs/fgd1fgd2Fit/errprop_fit1_fakedata_nuwro.json" >> job_xsllh_errprop_fit1_fakedata_nuwro.sh
+
+source /usr/local/shared/bin/openmpi_env.sh
+
+# Submit jobs
+
+qsub -l os=cl7,sps=1 -pe openmpi 8 -q pa_long -M maretlucie@gmail.com -m be job_xsllh_errprop_fit1_asimov.sh
+
+qsub -l os=cl7,sps=1 -pe openmpi 8 -q pa_long -M maretlucie@gmail.com -m be job_xsllh_errprop_fit3_statFluc.sh
+
+qsub -l os=cl7,sps=1 -pe openmpi 8 -q pa_long -M maretlucie@gmail.com -m be job_xsllh_errprop_fit1_fakedata_m30Carbon.sh
+qsub -l os=cl7,sps=1 -pe openmpi 8 -q pa_long -M maretlucie@gmail.com -m be job_xsllh_errprop_fit1_fakedata_p30Carbon.sh
+qsub -l os=cl7,sps=1 -pe openmpi 8 -q pa_long -M maretlucie@gmail.com -m be job_xsllh_errprop_fit1_fakedata_m30Oxygen.sh
+qsub -l os=cl7,sps=1 -pe openmpi 8 -q pa_long -M maretlucie@gmail.com -m be job_xsllh_errprop_fit1_fakedata_p30Oxygen.sh
+
+qsub -l os=cl7,sps=1 -pe openmpi 8 -q pa_long -M maretlucie@gmail.com -m be job_xsllh_errprop_fit1_fakedata_genie.sh
+qsub -l os=cl7,sps=1 -pe openmpi 8 -q pa_long -M maretlucie@gmail.com -m be job_xsllh_errprop_fit1_fakedata_nuwro.sh
+
+cd - 
+
+###########################################################
+
