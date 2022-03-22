3517212761dd7949ce16864443cc0b31a3f38263
Shilin Liu <shilin@ash.physics.sunysb.edu>
2022-03-22 12:30:34 -0400

    1. Modified /Applications/src/gundamFitter.cxx to enable mcmc option 2. Modified /Fitter/include/FitterEngine.h, fit() method is made virtual, so a derived class can redefine it 3. Created /Fitter/include/MCMCEngine.h & /Fitter/src/MCMCEngine.cpp, MCMCEngine class inherits from FitterEngine, it rewrites fit() function, in which a mcmc sampler is called to sample the likelihood 4. Created /Fitter/include/MCMCProxyEngine.h, a little trick to use intialized MCMCEngine in MCMC sampler suggested by Clark 5. Modified /Fitter/CMakeLists.txt to include new files

---
+++ src/Fitter/include/MCMCProxyEngine.h
@@ -0,0 +1,32 @@
+//
+// Created by Shilin on 03/18/2022
+//
+
+#ifndef GUNDAM_MCMCPROXYENGINE_H
+#define GUNDAM_MCMCPROXYENGINE_H
+
+#include "MCMCEngine.h"
+
+struct MCMCProxyEngine {
+    MCMCEngine *proxy;
+
+    // An operator that is needed as a likelihood calculator for TSimpleMCMC.H
+    // Will simply call FitterEngine::evalFit() method
+    double operator() (const std::vector<double>& fCurrParams) {
+
+	// FitterEngine::evalFit(const double* parArray_) does 2 things to calculate likelihood
+	// 1. It updates fit parameters _minimizerFitParameterPtr_ value with input parArray_
+	// 2. It calculates likelihood by calling FitterEngine::updateChi2Cache()
+	//    updateChi2Cache() does the "magic" to calculate _chi2Buffer_ with updated fit 
+	//    parameters _minimizerFitParameterPtr_.
+	// FitterEngine::evalFit(const double* parArray_) does a lot of things to output timing info
+	// --- FitterEngine.cpp:645 print _minimizerType_, _minimizerAlgo_ needs to be updated
+	// --- FitterEngine.cpp:653 _convergenceMonitor_, does it affect likelihood calculation?
+	// --- FitterEngine.cpp:668 _chi2HistoryTree_, does it affect likelihood calculation?
+        proxy->evalFit(&fCurrParams[0]);
+    }
+
+
+};
+
+#endif
---
+++ src/Fitter/src/MCMCEngine.cpp
@@ -0,0 +1,88 @@
+//
+// Created by Shilin on Mar.22, 2022
+//
+
+#include "MCMCEngine.h"
+
+#include "JsonUtils.h"
+
+void MCMCEngine::fit() {
+
+    // Update "minimizer" type and algo. Although MCMC is a sampler, we still use
+    // minimizer so we don't need to modify the base class FitterEngine
+    // This will be printed in evalFit() function
+    _minimizerType_ = "MCMC";
+    _minimizerAlgo_ = "Metropolis";
+
+    // Get output file name
+    string outFileName = JsonUtils::fetchValue(_minimizerConfig_, "mcmcOutputFile", "mcmc.root");
+    string outTreeName = JsonUtils::fetchValue(_minimizerConfig_, "mcmcOutputTree", "accepted");
+
+    // Create output file and a tree to save accepted points
+    TFile *outputFile = new TFile(outFileName.c_str(), "recreate");
+    TTree *tree = new TTree(outTreeName.c_str(),"Tree of accepted points");
+
+    // Create a mcmc sampler with this MCMCEngine serving as likelihood
+    TSimpleMCMC<MCMCProxyEngine> mcmc(tree);
+    ProxyEngine& like = mcmc.GetLogLikelihood();
+    // Let the MCMCEngine proxy pointer in ProxyEngine point to 'this' intialized object
+    like.proxy = this;
+
+    // Set dimensiton for the MCMC sampler
+    mcmc.GetProposeStep().SetDim(like.proxy->GetDim());
+
+    // Create a fitting parameter vector and intialize it
+    Vector p(like.proxy->GetDim());
+    for (int i = 0; i < _nbFitParameters_; i++) {
+        p[i] = _minimizerFitParameterPtr_[i]->getParameterValue();
+    }
+
+    // Get MCMC burnin and running info
+    int gBurninCycle = JsonUtils::fetchValue(_minimizerConfig_, "mcmcBurninCycle", 3);
+    int gBurninLength = JsonUtils::fetchValue(_minimizerConfig_, "mcmcBurninLength", 100);
+    int gRunCycle = JsonUtils::fetchValue(_minimizerConfig_, "mcmcRunCycle", 1);
+    int gRunLength = JsonUtils::fetchValue(_minimizerConfig_, "mcmcRunLength", 1000);
+    bool gSaveBurnin = JsonUtils::fetchValue(_minimizerConfig_, "mcmcSaveBurnin", false);
+
+    // Initializing the mcmc sampler
+    mcmc.Start(p, gSaveBurnin);
+
+    // Burnin cycles
+    for (int chain = 0; chain < gBurninCycle; ++i){
+      std::cout << "Start Burnin chain " << chain << std::endl;
+      mcmc.GetProposeStep().UpdateProposal();
+      // Burnin chain in each cycle
+      for (int i = 0; i < gBurninLength + p.size()*p.size(); ++i) {
+        // Run step
+        mcmc.Step(gSaveBurnin);
+
+        if(gBurninLength > 100 && !(i%(gBurninLength/100))){
+          std::cout << "On step: " << i+1 << "/" << gBurninLength << " "
+                    << i*100./gBurninLength << "%" << std::endl;
+        }
+      }
+    }
+    std::cout << "Finished burnin chains" << std::endl;
+
+    // Run cycles
+    for (int chain = 0; chain < gRunCycle; ++i){
+      std::cout << "Start run chain " << chain << std::endl;
+      mcmc.GetProposeStep().UpdateProposal();
+      // Run chain in each cycle
+      for (int i = 0; i < gRunStep + p.size()*p.size(); ++i) {
+        // Run step
+        mcmc.Step(true);
+
+        if(gRunLength > 100 && !(i%(gRunLength/100))){
+          std::cout << "On step: " << i+1 << "/" << gRunLength << " "
+                    << i*100./gRunLength << "%" << std::endl;
+        }
+      }
+    }
+    std::cout << "Finished Running chains" << std::endl;
+
+    // Save the sampled points to the outputfile
+    if (tree) tree->Write();
+    if (outputFile) delete outputFile;
+
+}
